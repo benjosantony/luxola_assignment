cad4997e763d09519a95412371311e03a0936ade
Benjos Antony Thareparmbil Joseph <benjosantony@gmail.com>
2015-02-03 01:04:53 +0800

    Alignment changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,29 +2,35 @@ class ProductsController < ApplicationController
   # Set the @product before running the actions
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
+  # GET /products
   def index
     @products  = Product.order 'id desc'
   end
 
+  # GET /products/1
   def  show
   end
 
+  # GET /products/new
   def new
     @product  = Product.new
   end
 
+  # POST /products
   def create
     @product = Product.new(product_params)
-      if @product.save
-        render :show
-      else
-        render :new
-      end
+    if @product.save
+      render :show
+    else
+      render :new
+    end
   end
 
+  # GET /products/1/edit
   def edit
   end
 
+  # PATCH/PUT /products/1
   def update
     if @product.update(product_params)
       render :show
@@ -40,14 +46,14 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.require(:product).permit(:name, :description, :price)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
+  end
 
 end
--- app/validators/money_validator.rb
+++ app/validators/money_validator.rb
@@ -1,16 +1,16 @@
 class MoneyValidator < ActiveModel::Validator
   def validate(record)
     if options[:fields].any? do |field|
-          # Check if number
-          value = record.send(field)
-          # only do the validation if something is entered
-          if !(value.is_a? NilClass)
-            # if either one of the condition is not met means improper money
-            if !(value.is_a?( Numeric)  && decimals(value.to_f()) <=2 && value.to_f() >=0)
-              record.errors[field] << " should be of proper cash value "
-            end
-          end
-       end
+      # Check if number
+      value = record.send(field)
+      # only do the validation if something is entered
+      if !(value.is_a? NilClass)
+        # if either one of the condition is not met means improper money
+        if !(value.is_a?( Numeric)  && decimals(value.to_f()) <=2 && value.to_f() >=0)
+          record.errors[field] << " should be of proper cash value "
+        end
+      end
+    end
     end
   end
 
@@ -24,7 +24,7 @@ class MoneyValidator < ActiveModel::Validator
     num
   end
 
-  end
+end
 
 
 
